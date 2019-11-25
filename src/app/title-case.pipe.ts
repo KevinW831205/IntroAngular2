7d93d3701797db408b8c9291a590cdf3e9e3979e
KevinW <bird831205@hotmail.com>
2019-11-24 20:51:37 -0500

    implement custom title case pipe

--- src/app/title-case.pipe.ts
+++ src/app/title-case.pipe.ts
@@ -5,8 +5,27 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TitleCasePipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+  transform(value: any): any {
+
+
+    if (!value) {
+      return null;
+    } else {
+      let words = value.split(" ");
+      let preposition = ['a','of','the','an','and'];
+
+      for (var i = 0; i < words.length; i++) {
+        if( preposition.includes(words[i]) && i != 0){
+          words[i] = words[i].toLowerCase();
+        } else {
+          words[i] = words[i].substr(0,1).toUpperCase()  + words[i].substr(1).toLowerCase();
+        }
+      }
+
+      return words.join(" ");
+    }
+
+
   }
 
 }
