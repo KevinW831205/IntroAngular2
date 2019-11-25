ff36f9d3e28a087686c79b2ae93c8458be89ed9c
KevinW <bird831205@hotmail.com>
2019-11-25 07:45:27 -0500

    create list from input

--- src/app/testground/testground.component.ts
+++ src/app/testground/testground.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestgroundComponent implements OnInit {
 
+  listInput = "";
+  list = [];
+
+
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  enterList(){
+    if(this.listInput){
+      let listItem = {
+        item: this.listInput,
+        date: new Date()
+      }
+
+      this.list.unshift(listItem);
+      console.log(this.list);
+
+
+      this.listInput = "";
+    }
+  }  
 
 }
