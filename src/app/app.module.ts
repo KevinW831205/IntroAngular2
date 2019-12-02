067b1b7b5c722a417286a0f8f266341d76bfd3d2
KevinW <bird831205@hotmail.com>
2019-12-02 12:30:45 -0500

    reactive forms creating controls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -44,7 +44,8 @@ import { SignupFormComponent } from './signup-form/signup-form.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    ReactiveFormsModule
   ],
   providers: [
     CoursesService
