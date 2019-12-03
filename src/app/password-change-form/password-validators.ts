35404419ccb38d536e72b313665af9516dfed6d7
KevinW <bird831205@hotmail.com>
2019-12-03 14:51:30 -0500

    defining validation method signatures

--- src/app/password-change-form/password-validators.ts
+++ src/app/password-change-form/password-validators.ts
@@ -1,5 +1,14 @@
 import { AbstractControl, ValidationErrors } from '@angular/forms';
 
 export class PasswordValidator{
+    static cannotBeOldPassword(control: AbstractControl): Promise<ValidationErrors> | null{
 
+        return null;
+    }
+
+    static notTheSamePassword(control: AbstractControl): ValidationErrors | null{
+
+        return null;
+    }
+    
 }
