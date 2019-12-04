b8e9c4aaee524ffd3b98d95885aa26b627df2c0a
KevinW <bird831205@hotmail.com>
2019-12-04 09:27:59 -0500

    delete

--- src/app/post-component/post-component.component.ts
+++ src/app/post-component/post-component.component.ts
@@ -28,18 +28,26 @@ export class PostComponentComponent implements OnInit {
     this.http.post(this.url, JSON.stringify(post))
       .subscribe(response => {
         post['id'] = response['id'];
-        this.posts.splice(0,0, post);
+        this.posts.splice(0, 0, post);
       })
   }
 
-  updatePost(post){
+  updatePost(post) {
     // this.http.put(this.url+"/"+post.id,post)
-    console.log(1)
-    this.http.patch(this.url+"/"+post.id, JSON.stringify({isRead : true}))
-      .subscribe(response =>{
+    console.log(post)
+    this.http.patch(this.url + "/" + post.id, JSON.stringify({ isRead: true }))
+      .subscribe(response => {
         console.log(response)
       })
+  }
 
+  deletePost(post) {
+    console.log(post)
+    this.http.delete(this.url + '/' + post.id)
+      .subscribe(response => {
+        let index = this.posts.indexOf(post);
+        this.posts.splice(index, 1)
+      })
   }
 
 }
