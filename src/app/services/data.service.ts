bc639a2866b294a3895e901ff6430b8c5ffd5830
KevinW <bird831205@hotmail.com>
2019-12-06 12:02:19 -0500

    reusable data service

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/operators'
+import { Observable } from 'rxjs-compat';
+import { AppError } from '../common/app-error';
+import { NotFoundError } from '../common/not-found-error';
+import { BadInput } from '../common/bad-input';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class DataService {
+
+
+    constructor(private http: HttpClient, private url: String) {
+    
+    }
+
+    getAll() {
+        return this.http.get(this.url)
+            .catch(this.hanldeError);
+    }
+
+    create(resource) {
+        return this.http.post(this.url, JSON.stringify(resource))
+            .catch(this.hanldeError)
+    }
+
+    update(resource) {
+        return this.http.patch(this.url + "/" + resource.id, JSON.stringify(resource))
+            .catch(this.hanldeError)
+    }
+
+    deletePost(id) {
+        return this.http.delete(this.url + "/" + id)
+            .catch(this.hanldeError);
+    }
+
+
+    private hanldeError(error: Response) {
+        if (error.status === 400) {
+            return Observable.throw(new BadInput(error.json()));
+        }
+        if (error.status === 404) {
+            return Observable.throw(new NotFoundError(error));
+        }
+        return Observable.throw(new AppError(error));
+    }
+}
--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -5,46 +5,19 @@ import { Observable } from 'rxjs-compat';
 import { AppError } from '../common/app-error';
 import { NotFoundError } from '../common/not-found-error';
 import { BadInput } from '../common/bad-input';
+import { DataService } from './data.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PostService {
+export class PostService extends DataService {
 
-  private url = "http://jsonplaceholder.typicode.com/posts"
+  // private url = "http://jsonplaceholder.typicode.com/posts"
   // +"brokenurlsdfsfdsfsd1!!?/sd";
 
 
-  constructor(private http: HttpClient) { }
-
-  getPost() {
-    return this.http.get(this.url)
-      .catch(this.hanldeError);
+  constructor(http: HttpClient) { 
+    super(http, "http://jsonplaceholder.typicode.com/posts");
   }
 
-  createPost(post) {
-    return this.http.post(this.url, JSON.stringify(post))
-      .catch(this.hanldeError)
-  }
-
-  updatePost(val, targetid) {
-    return this.http.patch(this.url + "/" + targetid, JSON.stringify(val))
-      .catch(this.hanldeError)
-  }
-
-  deletePost(targetid) {
-    return this.http.delete(this.url + "/" + targetid)
-      .catch(this.hanldeError);
-  }
-
-
-  private hanldeError(error: Response) {
-    if (error.status === 400) {
-      return Observable.throw(new BadInput(error.json()));
-    }
-    if (error.status === 404) {
-      return Observable.throw(new NotFoundError(error));
-    }
-    return Observable.throw(new AppError(error));
-  }
 }
